82de6c4ca5509284e126d3e5264685d6d5f27c23
Boris Dubois <78447@supinfo.com>
2011-02-23 18:22:41 -0500

    Correction class Lab Correction initial-data.yml modification data.yml (test)

--- app/models/Lab.java
+++ app/models/Lab.java
@@ -3,6 +3,7 @@ package models;
 import java.util.Collection;
 import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -15,16 +16,16 @@ public class Lab extends Model {
     @NoRender
     @OneToMany(mappedBy="lab")
     public List<User> members;
+    @NoRender
     @OneToOne
+    @JoinColumn
     public User glm;
-    
     
     public Lab(String name, User glm) {
         super();
         this.name = name;
         this.glm = glm;
     }
-
 
     public Lab(String name, List<User> members, User glm) {
         super();
